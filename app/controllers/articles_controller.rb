3a993768198ebd6e8646237abafe0939d662af2a
Dheeraj Thasma Ravindranath <dheerajthasmaravindranath@Mac-Dev-Dheeraj.local>
2018-09-13 18:34:31 +0530

    Add associations to user and articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,6 +15,7 @@ class ArticlesController < ApplicationController
 	def create
 		# render plain: params[:article].inspect # To display the list of values passed as a parameter on submitting the form
 		@article = Article.new(article_params)
+		@article.user = User.first
 		if @article.save
 			flash[:success] = "Article was successfully created!!!"
 			redirect_to article_path(@article)
